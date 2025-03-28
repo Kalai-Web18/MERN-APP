cf8bb38f5bb6bb109ffe07905cd28d8595cd6895
Kalai-Web18 <kalaithamizhkantha@gmail.com>
2025-03-28 16:03:02 +0530

    create a MERN-APP

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GridView from "../src/GridView";
+import Todo from "./Todo";
+import Navbar from "./Navbar";
+import ProfileCard from "./ProfileCard"
+import "./ProfileCard.jsx"
+import "./ProfileCard.css"; // Import CSS
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+     <ProfileCard/>
+      
+      <Routes>
+        <Route path="/" element={<Todo />} />
+        <Route path="/gridview" element={<GridView />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App;
+
